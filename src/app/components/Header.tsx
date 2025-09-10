7a293d00f86395a2759de83f2a205c65715ad81a
Petar Panajotov <petarpanajotov@gmail.com>
2025-09-10 20:26:46 +0300

    added twitch token handler

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,7 +1,8 @@
 'use client'
 
+import { getTwitchToken } from '@/lib/twitchToken'
 import Link from 'next/link'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 const links = [
   { href: '/', label: 'Home' },
