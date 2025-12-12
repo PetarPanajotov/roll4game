6cb48b40e7224d06c601c570210322d803bf84b2
Petar Panajotov <petarpanajotov@gmail.com>
2025-12-12 21:29:47 +0200

    type cast to any ignored

--- src/hooks/useFormState.ts
+++ src/hooks/useFormState.ts
@@ -1,7 +1,7 @@
 import { useCallback, useState } from 'react'
 
 type Updater<T> = T | ((prev: T) => T)
-
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function useFormState<T extends Record<string, any>>(initial: T) {
   const [form, setForm] = useState<T>(initial)
 
@@ -9,6 +9,7 @@ export function useFormState<T extends Record<string, any>>(initial: T) {
     <K extends keyof T>(key: K, value: Updater<T[K]>) => {
       setForm((prev) => ({
         ...prev,
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
         [key]: typeof value === 'function' ? (value as any)(prev[key]) : value,
       }))
     },
